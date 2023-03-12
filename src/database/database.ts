134aff33f81fccf67989c11733b47ddb22c6a849
Ponti-D <spaediaz@gmail.com>
2023-03-12 18:53:24 +0000

    Added .env.production file

--- src/database/database.ts
+++ src/database/database.ts
@@ -1,4 +1,21 @@
-import { Sequelize } from "sequelize";
+import { Dialect, Sequelize } from "sequelize";
+import * as dotenv from "dotenv";
 
 // TODO: This should be external config
-export const sequelize = new Sequelize("sqlite::memory:");
+export let sequelize = new Sequelize("sqlite::memory:");
+
+const environment = process.env.NODE_ENV?.trim() || "dev";
+dotenv.config({  path: `.env.${environment}` });
+
+if (process.env.NODE_ENV?.trim() !== "test") {
+	sequelize = new Sequelize(
+		process.env.DB_NAME ?? "MISSING_DB_NAME_CONFIG",
+		process.env.DB_USERNAME ?? "MISSING_DB_USERNAME_CONFIG",
+		process.env.DB_PASSWORD ?? "MISSING_DB_PASSWORD_CONFIG",
+		{
+			host: process.env.DB_HOST ?? "MISSING_DB_HOST_CONFIG",
+			port: parseInt(process.env.DB_PORT as string) ??  "MISSING_DB_PORT_CONFIG",
+			dialect: (process.env.DB_DIALECT as Dialect) ?? "postgres",
+		}
+	);
+}
--- src/server.ts
+++ src/server.ts
@@ -2,17 +2,15 @@ import * as dotenv from "dotenv";
 import { app } from "./app";
 import { populateDummyData } from "./database/database_seed";
 
-const environment = process.env.NODE_ENV || "dev";
-dotenv.config({ path: `.env.${environment}` });
+const environment = process.env.NODE_ENV?.trim() || "dev";
+dotenv.config({  path: `.env.${environment}` });
 
-const PORT = process.env.PORT;
+const PORT =process.env.PORT;
 
 console.log(`🌍 Running in ${environment} environment`);
-
 app.listen(PORT, () => {
 	console.log(`🚂 Express started on port ${PORT}`);
 
-	// Seed the database with some data
 	if (environment === "dev") {
 		populateDummyData();
 	}
